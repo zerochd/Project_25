3286200b6275ffedbde2f718860c952f989fecba
zerochd <zerochd@qq.com>
2017-06-11 13:38:20 +0800

    init

---
+++ Assets/_Project/Scripts/Actor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour {
+
+	public Head head;
+	public Heart heart;
+	public Body body;
+	public Hand hand;
+	public Leg leg;
+
+	void Update(){
+	}
+
+	void Move(){
+		
+	}
+}
---
+++ Assets/_Project/Scripts/Part/Part.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Part : MonoBehaviour {
+
+	public virtual void Connect(Actor owner){
+	
+	}
+
+	public virtual void DisConnect(){
+	
+	}
+
+	public virtual void Break(){
+		
+	}
+}
